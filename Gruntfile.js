0fd1bc59d71eabc31e94965100b82fc121b3b6b3
zhangshuo <443494891@qq.com>
2020-03-04 09:55:25 +0800

    CSS压缩代码

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,10 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    uglify: {
-      release:{
-        files: {
-          'js/jquery.min.js': 'js/jquery.js'
-        }
-      }       
+    cssmin: {  
+      'rectangle.min.css': 'rectangle.css'
     }
   });
-    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['uglify:release']);
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['cssmin']); 
 };
