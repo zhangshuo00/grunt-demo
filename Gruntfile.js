4e058e0d1965711eb6cbff4dbd9d326796f1cc1c
zhangshuo <443494891@qq.com>
2020-03-04 09:10:49 +0800

    压缩图片

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,15 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    mochacli: {
-      options: {
-        reporter: 'spec',
-        bail: true
-      },
-      all: ['test/*.js']
-    },
-    mocha_istanbul: {
-      coverage: {
-        src: 'test'
-      }
-    },
-    istanbul_check_coverage: {
-      default: {
-        options: {
-          coverageFolder: 'coverage*',
-          check: {
-            lines: 90,
-            statements: 90
-          }
-        }
+    imagemin: {
+      files: {
+        expand: true,
+        src: ['images/*.{png,jpg,gif}'],
+        dest: 'dist/'
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadNpmTasks('grunt-mocha-istanbul');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['mochacli']);   
-  grunt.registerTask('cover', ['mocha_istanbul']);
-  grunt.registerTask('check-cover', ['istanbul_check_coverage']);
+  grunt.registerTask('default', ['imagemin']);
 };
