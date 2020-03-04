fe50e7c612e46384d8144ae62110c47ec7ab8473
zhangshuo <443494891@qq.com>
2020-03-04 09:25:26 +0800

    JavaScript压缩代码

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,14 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    imagemin: {
-      files: {
-        expand: true,
-        src: ['images/*.{png,jpg,gif}'],
-        dest: 'dist/'
-      }
+    uglify: {
+      release:{
+        files: {
+          'js/jquery.min.js': 'js/jquery.js'
+        }
+      }       
     }
   });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-
-  grunt.registerTask('default', ['imagemin']);
+    grunt.registerTask('default', ['uglify:release']);
 };
